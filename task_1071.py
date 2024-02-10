116c0cc1ce12b18a1968aa4d6c2ac92be8386542
Szymon Cwiertnia <szymon.cwiertnia@techsterowniki.pl>
2024-02-10 07:40:48 +0100

    version 41 passed

--- task_1071.py
+++ task_1071.py
@@ -4,23 +4,34 @@ str28 = "ABC"
 
 str122 = "ABABAB"
 str221 = "ABAB"
-str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX"
-str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+str1234 = "TAUXXTAUXXTAUXXTAUXXTAUXX"
+str2324 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+str1 = "CXTXNCXTXNCXTXNCXTXNCXTXN"
+str2 = "CXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXN"
 
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
 
         output = []
-
+        id = 0
+        lst_id = 0
+        first: str = None
         x = zip(reversed(str1), reversed(str2))
         for char1, char2 in x:
-
             if char1 == char2:
-                if char1 in output:
-                    output.append(char1)
+                if char1 in output and char1 != first:
+                    id += 1
+                    continue
+                elif id != lst_id:
+                    break
+
                 else:
                     output.append(char1)
+                    first = output[-1]
+                    id += 1
+                    lst_id = id
+
         output = reversed(output)
         return "".join(output)
 
